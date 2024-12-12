a19f8ab2a09b92575a103e09a45c20d4efeb243c
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-12-12 15:55:41 +0100

    Update Main.java

--- src/main/java/com/openwar/openwarlevels/Main.java
+++ src/main/java/com/openwar/openwarlevels/Main.java
@@ -1,5 +1,6 @@
 package com.openwar.openwarlevels;
 
+import com.openwar.openwarcore.Utils.LevelSaveAndLoadBDD;
 import com.openwar.openwarfaction.factions.FactionManager;
 import com.openwar.openwarlevels.GUI.ItemBuilder;
 import com.openwar.openwarlevels.GUI.LevelGUI;
@@ -18,11 +19,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.UUID;
 
 public final class Main extends JavaPlugin {
-    private PlayerDataManager pl;
+    private LevelSaveAndLoadBDD pl;
     private FactionManager fm;
 
     private boolean setupDepend() {
-        RegisteredServiceProvider<PlayerDataManager> levelProvider = getServer().getServicesManager().getRegistration(PlayerDataManager.class);
+        RegisteredServiceProvider<LevelSaveAndLoadBDD> levelProvider = getServer().getServicesManager().getRegistration(LevelSaveAndLoadBDD.class);
         RegisteredServiceProvider<FactionManager> factionDataProvider = getServer().getServicesManager().getRegistration(FactionManager.class);
         if (levelProvider == null || factionDataProvider == null) {
             System.out.println("ERROR !!!!!!!!!!!!!!!!!!!!");
