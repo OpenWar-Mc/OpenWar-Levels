bdf82d5b8fddc529bdaac441aea2dc5a98a047be
Charpy <76815325+Charpyy@users.noreply.github.com>
2025-03-12 12:38:27 +0100

    Update LevelLock.java

--- src/main/java/com/openwar/openwarlevels/handlers/LevelLock.java
+++ src/main/java/com/openwar/openwarlevels/handlers/LevelLock.java
@@ -12,7 +12,6 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
@@ -22,7 +21,6 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class LevelLock implements Listener {
     private LevelSaveAndLoadBDD data;
